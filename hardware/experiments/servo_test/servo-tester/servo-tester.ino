a363b8b26ba2bdfd11ccc38758f79616371eb270
Sam Culp <samculp04@yahoo.com>
2026-04-10 15:08:26 -0500

    initial servo plunger test

---
+++ hardware/experiments/servo_test/servo-tester/servo-tester.ino
@@ -0,0 +1,23 @@
+#include <Servo.h>
+
+Servo servo;
+
+const int SERVO_PIN = 9;
+const int REST_ANLGE = 90;
+const int PRESS_ANGLE = 110;
+
+void setup() {
+  servo.attach(SERVO_PIN);
+  servo.write(REST_ANGLE);
+  delay(1000);
+}
+
+void loop() {
+  // press
+  servo.write(PRESS_ANLGLE);
+  delay(100);
+
+  // release
+  servo.write(REST_ANGLE);
+  delay(1000);
+}
