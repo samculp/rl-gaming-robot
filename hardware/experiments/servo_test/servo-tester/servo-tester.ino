74d7f0220b82aadd9a0ac1618369d2a11a6228cb
Sam Culp <samculp04@yahoo.com>
2026-04-14 19:42:10 -0500

    servo test and cradle STL file

--- hardware/experiments/servo_test/servo-tester/servo-tester.ino
+++ hardware/experiments/servo_test/servo-tester/servo-tester.ino
@@ -3,8 +3,8 @@
 Servo servo;
 
 const int SERVO_PIN = 9;
-const int REST_ANLGE = 90;
-const int PRESS_ANGLE = 110;
+const int REST_ANGLE = 90;
+const int PRESS_ANGLE = 120;
 
 void setup() {
   servo.attach(SERVO_PIN);
@@ -13,11 +13,16 @@ void setup() {
 }
 
 void loop() {
+  pressButton();
+  delay(1200); 
+}
+
+void pressButton() {
   // press
-  servo.write(PRESS_ANLGLE);
+  servo.write(PRESS_ANGLE);
   delay(100);
 
   // release
   servo.write(REST_ANGLE);
-  delay(2000);
+  delay(100);
 }
